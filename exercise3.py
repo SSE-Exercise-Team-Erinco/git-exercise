0cd14ffa27b308ab2f4d55e07b54c4b64af351f1
erinc-emre <erinc-emre@outlook.com>
2025-10-18 15:50:44 +0200

    feat: refactor Rectangle.contains method to use is_in_interval for boundary checks

--- exercise3.py
+++ exercise3.py
@@ -26,14 +26,11 @@ class Rectangle:
         return self.corner(3)
 
     def contains(self, point: Point2D, tolerance: float = 0.0) -> bool:
-        # Task A: remove duplication by defining a function
-        #         that checks if a value is within an interval
-        #         and reuse that here.
         tolerance = abs(tolerance)
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return  ll_px >= -tolerance and ll_px <= self._dx + tolerance \
-            and ll_py >= -tolerance and ll_py <= self._dy + tolerance
+        return self.is_in_interval(ll_px, self._dx, tolerance) \
+            and self.is_in_interval(ll_py, self._dy, tolerance)
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
@@ -41,7 +38,8 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
-    # def is_in_interval(...) -> bool: # Task A
+    def is_in_interval(self, value: float, interval_size: float, tolerance: float = 0.0) -> bool:
+        return value >= -tolerance and value <= interval_size + tolerance 
 
 
 def test_rectangle_contains_exact() -> None:
